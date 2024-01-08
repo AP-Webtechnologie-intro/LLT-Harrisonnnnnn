c3e634f7083d0168c9cd4956d314911593d18d86
Isokpan Harrison (s144184) <harrison.isokpan@student.ap.be>
2024-01-08 16:43:56 +0100

    upload pagina's en mappen

---
+++ map.js
@@ -0,0 +1,9 @@
+// map.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  var mymap = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+  }).addTo(mymap);
+});
