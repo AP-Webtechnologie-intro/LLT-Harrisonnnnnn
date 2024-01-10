da9270ae429a1e74af0cecc4a3b80ba884b5eb1e
Isokpan Harrison (s144184) <harrison.isokpan@student.ap.be>
2024-01-10 12:14:36 +0100

    Heb de coordinaten toegevoegd van Ap en een marker

--- map.js
+++ map.js
@@ -1,9 +1,13 @@
-// map.js
-
 document.addEventListener("DOMContentLoaded", function () {
-  var mymap = L.map("map").setView([51.505, -0.09], 13);
+  var mymap = L.map("map").setView([51.230474048870356, 4.414275972993863], 13);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors",
   }).addTo(mymap);
+
+  // Voeg een marker toe op de opgegeven coördinaten
+  var marker = L.marker([51.230474048870356, 4.414275972993863]).addTo(mymap);
+
+  // Voeg eventueel een pop-up toe aan de marker
+  marker.bindPopup("AP Hogeschool").openPopup();
 });
